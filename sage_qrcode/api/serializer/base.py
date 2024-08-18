da7b48b638528dc366250f007f2569a17ca179de
radinceorc <radin.ceorc77@gmail.com>
2024-08-19 00:36:57 +0330

    build(sage_qrcode): Add check.py to make sure every thing is set correct

--- sage_qrcode/api/serializer/base.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from sage_qrcode.models import QRCode
-
-
-class QRCodeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QRCode
-        fields = "__all__"
--- sage_qrcode/apps.py
+++ sage_qrcode/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class QrcodeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sage_qrcode"
+
+    def ready(self) -> None:
+        import sage_qrcode.check
